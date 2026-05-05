20a6a24abb1ec46a8666da3954ec321b21a2f807
Carlos Delfino <consultoria@carlosdelfino.eti.br>
2026-05-05 19:15:22 -0300

    Ajuste na tradução

--- src/App.jsx
+++ src/App.jsx
@@ -91,13 +91,25 @@ function AppContent() {
             <div className="visitor-counter">
               <img 
                 src="https://visitor-badge.laobi.icu/badge?page_id=carlosdelfino.fisica-dos-semicondutores.site" 
-                alt="Visitantes"
+                alt={t('header.visitors')}
                 className="visitor-badge"
               />
             </div>
           </div>
         </div>
       </header>
+
+      {/* Tarja GitHub com i18n */}
+      <aside className="gh-ribbon" aria-label={t('menu.items.community')}>
+        <a href="https://github.com/carlosdelfino/fisica-dos-semicondutores"
+           target="_blank" rel="noopener noreferrer"
+           title={t('menu.items.community_title')}>
+          <svg viewBox="0 0 24 24" aria-hidden="true">
+            <path d="M12 .5C5.65.5.5 5.65.5 12c0 5.08 3.29 9.39 7.86 10.91.58.1.79-.25.79-.56v-2.13c-3.2.7-3.87-1.36-3.87-1.36-.52-1.32-1.27-1.67-1.27-1.67-1.04-.71.08-.7.08-.7 1.15.08 1.76 1.18 1.76 1.18 1.02 1.75 2.68 1.24 3.34.95.1-.74.4-1.24.72-1.53-2.55-.29-5.24-1.28-5.24-5.69 0-1.26.45-2.28 1.18-3.09-.12-.29-.51-1.46.11-3.04 0 0 .96-.31 3.15 1.18.91-.25 1.89-.38 2.86-.38.97 0 1.95.13 2.86.38 2.18-1.49 3.14-1.18 3.14-1.18.62 1.58.23 2.75.11 3.04.74.81 1.18 1.83 1.18 3.09 0 4.42-2.69 5.39-5.26 5.68.41.36.78 1.06.78 2.13v3.16c0 .31.21.66.8.55C20.21 21.39 23.5 17.08 23.5 12 23.5 5.65 18.35.5 12 .5z"/>
+          </svg>
+          <span>{t('menu.items.community')}</span>
+        </a>
+      </aside>
 
       <aside className="sidebar">
         <ControlPanel
